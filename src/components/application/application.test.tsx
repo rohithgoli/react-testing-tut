82f1d6511aca9eda70928bba5c4c52f27195517b
rohith <rohith.goli@anywhere.co>
2023-03-26 23:24:36 +0530

    getByLabelText query & options

--- src/components/application/application.test.tsx
+++ src/components/application/application.test.tsx
@@ -20,6 +20,11 @@ describe('Application', () => {
         }); // an input element has default role called textbox
         expect(nameElement).toBeInTheDocument();
 
+        const nameElement2 = screen.getByLabelText("Name", {
+            selector: 'input'
+        });
+        expect(nameElement2).toBeInTheDocument();
+
         const bioElement = screen.getByRole("textbox", {
             name: 'Bio'
         });
@@ -30,6 +35,9 @@ describe('Application', () => {
 
         const termsElement = screen.getByRole('checkbox');
         expect(termsElement).toBeInTheDocument();
+
+        const termsElement2 = screen.getByLabelText('I agree to the terms and conditions')
+        expect(termsElement2).toBeInTheDocument();
 
         const submitButtonElement = screen.getByRole('button');
         expect(submitButtonElement).toBeInTheDocument();
