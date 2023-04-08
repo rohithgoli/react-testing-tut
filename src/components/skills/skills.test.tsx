d67c9d31e4105695c56c73b44afbbe85cfc7a29d
rohith <rohith.goli@anywhere.co>
2023-04-08 17:05:51 +0530

    manual queries, debugging

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -1,4 +1,4 @@
-import {render, screen} from '@testing-library/react'
+import {render, screen, logRoles} from '@testing-library/react'
 import {Skills} from './skills'
 
 describe('Skills', () => {
@@ -33,7 +33,9 @@ describe('Skills', () => {
     })
 
     test('Start learning button is eventually displayed', async () => {
-        render(<Skills skills={skills}/>);
+        const view = render(<Skills skills={skills}/>);
+        logRoles(view.container);
+        // screen.debug();
         const startLearningButton = await screen.findByRole('button', {
             name: 'Start learning'
         },
