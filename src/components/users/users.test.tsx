4c9819241adbd970dcc1a219f0a225d12a8ce5f1
rohith <rohith.goli@anywhere.co>
2023-04-10 00:05:25 +0530

    mocking http requests

--- src/components/users/users.test.tsx
+++ src/components/users/users.test.tsx
@@ -7,4 +7,10 @@ describe("Users", () => {
         const textElement = screen.getByText("Users");
         expect(textElement).toBeInTheDocument();
     });
+
+    test('renders a list of users', async () => {
+        render(<Users />)
+        const users = await screen.findAllByRole('listitem')
+        expect(users).toHaveLength(3);
+    })
 });
