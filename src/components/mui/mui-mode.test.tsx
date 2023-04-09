b30a392fadabace88acf4a0cf873359a64c0af48
rohith <rohith.goli@anywhere.co>
2023-04-09 21:53:02 +0530

    custom render for provider

--- src/components/mui/mui-mode.test.tsx
+++ src/components/mui/mui-mode.test.tsx
@@ -1,5 +1,4 @@
-import {render, screen} from '@testing-library/react'
-import { AppProviders } from '../../providers/app-providers'
+import {render, screen} from '../../test-utils'
 import { MuiMode } from './mui-mode'
 
 describe('MuiMode', () => {
@@ -12,9 +11,7 @@ describe('MuiMode', () => {
     // How do we test this component that is wrapped in a provider ?
 
     test('renders text correctly', () => {
-        render(<MuiMode/>, {
-            wrapper: AppProviders
-        })
+        render(<MuiMode/>)
         const headingElement = screen.getByRole('heading');
         expect(headingElement).toHaveTextContent('dark mode');
     })
