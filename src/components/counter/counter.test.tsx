aaa6753bf88ab40387efdfda3ba9be518e12df42
rohith <rohith.goli@anywhere.co>
2023-04-09 16:37:49 +0530

    user-event test keyboard interactions

--- src/components/counter/counter.test.tsx
+++ src/components/counter/counter.test.tsx
@@ -42,5 +42,37 @@ describe('Counter', () => {
         await user.click(incrementButton);
         const countElement = screen.getByRole('heading');
         expect(countElement).toHaveTextContent('2');
+    });
+
+    test('renders a count of 10 after clicking the set button', async () => {
+        user.setup();
+        render(<Counter />)
+        const amountInput = screen.getByRole('spinbutton');
+        await user.type(amountInput, '10')
+        expect(amountInput).toHaveValue(10)
+        const setButton = screen.getByRole('button', {
+            name: 'Set'
+        });
+        await user.click(setButton)
+        const countElement = screen.getByRole('heading');
+        expect(countElement).toHaveTextContent('10');
+    });
+
+    test('elements are focussed in the right order', async () => {
+        user.setup();
+        render(<Counter/>)
+        const amountInput = screen.getByRole('spinbutton');
+        const incrementButton = screen.getByRole('button', {
+            name: 'Increment'
+        });
+        const setButton = screen.getByRole('button', {
+            name: 'Set'
+        });
+        await user.tab()
+        expect(incrementButton).toHaveFocus()
+        await user.tab()
+        expect(amountInput).toHaveFocus()
+        await user.tab()
+        expect(setButton).toHaveFocus()
     })
 })
