9d162d8673573167ee4e643663c8e94e3b357077
rohith <rohith.goli@anywhere.co>
2023-04-09 23:38:52 +0530

    intro to mock http requests

---
+++ src/components/users/users.test.tsx
@@ -0,0 +1,10 @@
+import {render, screen} from '@testing-library/react';
+import { Users } from './users';
+
+describe("Users", () => {
+    test("renders correctly", () => {
+        render(<Users />)
+        const textElement = screen.getByText("Users");
+        expect(textElement).toBeInTheDocument();
+    });
+});
