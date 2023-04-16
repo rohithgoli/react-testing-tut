848df0757a991351950dde7b4c38b56faae121ac
rohith <rohith.goli@anywhere.co>
2023-04-16 17:26:58 +0530

    msw error handling

--- src/components/users/users.test.tsx
+++ src/components/users/users.test.tsx
@@ -1,6 +1,7 @@
 import {render, screen} from '@testing-library/react';
+import {rest} from 'msw';
 import { Users } from './users';
-
+import { server } from '../../mocks/server';
 describe("Users", () => {
     test("renders correctly", () => {
         render(<Users />)
@@ -12,5 +13,19 @@ describe("Users", () => {
         render(<Users />)
         const users = await screen.findAllByRole('listitem')
         expect(users).toHaveLength(3);
+    })
+
+    test('renders error', async () => {
+        server.use(
+            rest.get(
+                'https://jsonplaceholder.typicode.com/users',
+                (req, res, ctx) => {
+                    return res(ctx.status(500))
+                }
+            )
+        )
+        render(<Users/>)
+        const error = await screen.findByText('Error fetching users');
+        expect(error).toBeInTheDocument();
     })
 });
